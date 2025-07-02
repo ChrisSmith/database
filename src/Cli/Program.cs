467cda458c63947cff61b058e1d73344663c3b01
ChrisSmith <chris.smith324@gmail.com>
2025-07-02 10:17:08 -0400

    add makefile

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,32 +1,36 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Database.Core;
 using Database.Core.Catalog;
 using Database.Core.Execution;
 using Database.Core.Planner;
 
 var catalog = new Catalog();
+
+var homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+var dataPath = Path.Combine(homeDir, "src/database/data.parquet");
+
 catalog.Tables.Add(new TableSchema("table", new List<ColumnSchema>
 {
     new("Id", DataType.Int),
     new("Unordered", DataType.Int),
     new("Name", DataType.String),
-}, "/home/chris/src/database/data.parquet"));
-        
+}, dataPath));
+
 var planner = new QueryPlanner(catalog);
 
 var sw = Stopwatch.StartNew();
-        
+
 var scanner = new Scanner("SELECT Id, Unordered, Name FROM table;");
 var tokens = scanner.ScanTokens();
 var parser = new Parser(tokens);
 var statement = parser.Parse();
 
 var plan = planner.CreatePlan(statement);
-        
+
 var it = new Interpreter();
 var result = it.Execute(plan).ToList();
 var numRows = result.Sum(r => r.Columns[0].Length);
-    
+
 sw.Stop();
 
 var rg = result[0];
@@ -34,9 +38,9 @@ var rg = result[0];
 var columnHeader = string.Join(", ", rg.ColumnNames);
 Console.WriteLine(columnHeader);
 
-for(var row = 0; row < 10; row++)
+for (var row = 0; row < 10; row++)
 {
-    for(var col = 0; col < rg.Columns.Count; col++)
+    for (var col = 0; col < rg.Columns.Count; col++)
     {
         switch (rg.Columns[col])
         {
@@ -47,7 +51,7 @@ for(var row = 0; row < 10; row++)
                 Console.Write(c.Values[row]);
                 break;
         }
-        
+
         if (col < rg.Columns.Count - 1)
         {
             Console.Write(", ");
