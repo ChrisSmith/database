1e64030d0e188d2f0b2e880a80f60e500fd08ef0
ChrisSmith <chris.smith324@gmail.com>
2025-07-14 13:40:44 -0400

    basic terminal history + line editing

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -22,12 +22,21 @@ var planner = new QueryPlanner(catalog);
 
 var previousLines = new List<string>();
 
+// Load history from ~/.cache/databasecli.txt
+var cacheDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".cache");
+var historyPath = Path.Combine(cacheDir, "databasecli.txt");
+if (File.Exists(historyPath))
+{
+    previousLines = File.ReadAllLines(historyPath).TakeLast(100).ToList();
+}
+Directory.CreateDirectory(cacheDir);
+
 while (true)
 {
     try
     {
         Console.Write("> ");
-        var query = Console.ReadLine();
+        var query = ReadLineWithHistory(previousLines);
         if (string.IsNullOrWhiteSpace(query))
         {
             continue;
@@ -37,6 +46,12 @@ while (true)
             break;
         }
         previousLines.Add(query);
+
+        if (previousLines.Count > 100)
+        {
+            previousLines.RemoveAt(0);
+        }
+        File.WriteAllLines(historyPath, previousLines);
 
         var sw = Stopwatch.StartNew();
 
@@ -95,6 +110,132 @@ void PrintTable(List<RowGroup> result)
         }
         Console.WriteLine();
     }
+}
+
+string ReadLineWithHistory(List<string> previousLines)
+{
+    var input = new List<char>();
+    var historyIndex = previousLines.Count;
+    var currentInput = "";
+    int cursorPos = 0;
+    int prevRenderLength = 0;
+
+    void RenderInput()
+    {
+        int clearLength = Math.Max(prevRenderLength, input.Count);
+        Console.Write("\r> " + new string(' ', clearLength));
+        Console.Write("\r> " + new string(input.ToArray()));
+        int endPos = input.Count;
+        if (cursorPos < endPos)
+        {
+            int moveLeft = endPos - cursorPos;
+            if (moveLeft > 0)
+            {
+                Console.Write($"\u001b[{moveLeft}D");
+            }
+        }
+        prevRenderLength = input.Count;
+    }
+
+    while (true)
+    {
+        var keyInfo = Console.ReadKey(intercept: true);
+
+        if (keyInfo.Key == ConsoleKey.Enter)
+        {
+            Console.WriteLine();
+            break;
+        }
+        else if (keyInfo.Key == ConsoleKey.UpArrow)
+        {
+            if (previousLines.Count > 0 && historyIndex > 0)
+            {
+                historyIndex--;
+                input = previousLines[historyIndex].ToList();
+                cursorPos = input.Count;
+                currentInput = new string(input.ToArray());
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.DownArrow)
+        {
+            if (previousLines.Count > 0 && historyIndex < previousLines.Count - 1)
+            {
+                historyIndex++;
+                input = previousLines[historyIndex].ToList();
+                cursorPos = input.Count;
+                currentInput = new string(input.ToArray());
+                RenderInput();
+            }
+            else if (historyIndex == previousLines.Count - 1)
+            {
+                historyIndex++;
+                input = new List<char>();
+                cursorPos = 0;
+                currentInput = "";
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.Backspace)
+        {
+            if (cursorPos > 0)
+            {
+                input.RemoveAt(cursorPos - 1);
+                cursorPos--;
+                currentInput = new string(input.ToArray());
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.Delete)
+        {
+            if (cursorPos < input.Count)
+            {
+                input.RemoveAt(cursorPos);
+                currentInput = new string(input.ToArray());
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.LeftArrow)
+        {
+            if (cursorPos > 0)
+            {
+                cursorPos--;
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.RightArrow)
+        {
+            if (cursorPos < input.Count)
+            {
+                cursorPos++;
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.Home)
+        {
+            if (cursorPos != 0)
+            {
+                cursorPos = 0;
+                RenderInput();
+            }
+        }
+        else if (keyInfo.Key == ConsoleKey.End)
+        {
+            if (cursorPos != input.Count)
+            {
+                cursorPos = input.Count;
+                RenderInput();
+            }
+        }
+        else if (!char.IsControl(keyInfo.KeyChar))
+        {
+            input.Insert(cursorPos, keyInfo.KeyChar);
+            cursorPos++;
+            currentInput = new string(input.ToArray());
+            RenderInput();
+        }
+    }
+    return new string(input.ToArray());
 }
 
 
